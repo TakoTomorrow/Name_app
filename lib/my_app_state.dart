5caff281e3e932b8fbbfd0785ee1a22c806c56a7
TakoTomorrow <96362669+TakoTomorrow@users.noreply.github.com>
2023-03-08 13:27:56 +0800

    暫存

--- lib/my_app_state.dart
+++ lib/my_app_state.dart
@@ -9,8 +9,15 @@ class MyAppState extends ChangeNotifier {
   /// 我的最愛清單
   var favorites = <WordPair>[];
 
+  var histories = <WordPair>[]; 
+
+  GlobalKey? historyListKey ;
+
   /// 取得下一個
   void getNext() {
+    histories.insert(0,current);
+    var animatedList = historyListKey?.currentState as AnimatedListState?;
+    animatedList?.insertItem(0);
     current = WordPair.random();
 
     // 通知監聽者
@@ -18,12 +25,21 @@ class MyAppState extends ChangeNotifier {
   }
 
   /// 切換我的最愛
-  void toggleFavorite() {
+  void toggleFavorite([WordPair? pair]) {
+    pair = pair ?? current;
+
     if (favorites.contains(current)) {
       favorites.remove(current);
     } else {
       favorites.add(current);
     }
+
+    // 通知監聽者
+    notifyListeners();
+  }
+
+  void removeFavorite({required WordPair pair}){
+    favorites.remove(pair);
 
     // 通知監聽者
     notifyListeners();
